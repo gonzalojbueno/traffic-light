9917300c09fecfb84a5ba5127880d08baf379e7b
gonzalojbueno <gonzalojbueno@gmail.com>
2020-01-23 11:58:16 -0300

    Traffic-light-gonza-done

--- src/components/TrafficLight.jsx
+++ src/components/TrafficLight.jsx
@@ -5,9 +5,9 @@ class TrafficLight extends React.Component {
     constructor(){
         super();
           this.state = {
-            estadoRoja: "off",
-            estadoAmarilla: "off",
-            estadoVerde: "off"
+            redState: "off",
+            yellowState: "off",
+            greenState: "off"
           };
       }
 
@@ -16,14 +16,14 @@ class TrafficLight extends React.Component {
         return <div>
             <div id="palo"></div>
             <div id="contenedor">
-            <div className= "luz roja" id= {this.state.estadoRoja} 
-            onClick={()=>this.setState({estadoRoja: "on", estadoAmarilla: "off", estadoVerde: "off"})}>
+            <div className= "luz roja" id= {this.state.redState} 
+            onClick={()=>this.setState({redState: "on", yellowState: "off", greenState: "off"})}>
             </div>
-            <div className= "luz amarilla" id= {this.state.estadoAmarilla} 
-            onClick={()=>this.setState({estadoRoja: "off", estadoAmarilla: "on", estadoVerde: "off"})}>
+            <div className= "luz amarilla" id= {this.state.yellowState} 
+            onClick={()=>this.setState({redState: "off", yellowState: "on", greenState: "off"})}>
             </div>
-            <div className= "luz verde" id= {this.state.estadoVerde} 
-            onClick={()=>this.setState({estadoRoja: "on", estadoAmarilla: "off", estadoVerde: "on"})}>
+            <div className= "luz verde" id= {this.state.greenState} 
+            onClick={()=>this.setState({redState: "on", yellowState: "off", greenState: "on"})}>
             </div>
 
             </div>
